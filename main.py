a4d9bf17dc08d528baa22b4e27206f3c0adf5b69
Paul Otieno <vitalispaul48@live.com>
2025-12-07 07:44:22 +0300

    Sample get requests

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
 @app.get('/')
 async def read_root():
     return {"message": "Hello world!"}
+
+@app.get('/greet/{username}')
+async def greet(username:str):
+    return {"message": f"Hello {username}"}
+
+@app.get('/name/{newOne}')
+async def paul(newOne:str):
+    return {"message": f"New {newOne}"}
+
+user_list = [
+    "Jerry",
+    "Joey",
+    "Phil"
+]
+
+@app.get('/search')
+async def search_for_user(username:str):
+    for _ in user_list:
+        if username in user_list:
+            return {"message": f"details for user {username}"}
+        else:
+            return {"message": "User Not found"}
+
+@app.get('/hello/')
+async def hello(username:Optional[str]="User"):
+    return {"message": f"Hello {username}"}
