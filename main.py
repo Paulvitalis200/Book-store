97ee2dc36fb41340754b835118f3be1678e1dab2
Paul Otieno <vitalispaul48@live.com>
2025-12-07 07:58:55 +0300

    Add Post methods

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -32,3 +33,58 @@ async def search_for_user(username:str):
 @app.get('/hello/')
 async def hello(username:Optional[str]="User"):
     return {"message": f"Hello {username}"}
+
+
+class UserSchema(BaseModel):
+    username: str
+    email: str
+
+class BookSchema(BaseModel):
+    title: str
+    quantity: int
+
+users = []
+books = []
+
+@app.post("/create_user")
+async def create_user(user_data: UserSchema):
+    new_user = {
+        "username": user_data.username,
+        "email": user_data.email
+    }
+
+    users.append(new_user)
+
+    return {
+        "message": "User Created Successfully",
+        "user": new_user
+    }
+
+@app.get("/users")
+async def get_users():
+    return {
+        "message": "Users returned successfully",
+        "users": users
+    }
+
+@app.post("/create_book")
+async def create_book(book_data: BookSchema):
+    book = {
+        "title": book_data.title,
+        "quantity": book_data.quantity
+    }
+
+    books.append(book)
+
+    return {
+        "message": "Book Added successfully",
+        "book": book
+    }
+
+
+@app.get("/books")
+async def get_books():
+    return {
+        "message": "Books retrieved",
+        "books": books
+    }
