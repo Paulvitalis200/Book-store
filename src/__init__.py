cc7d754399ddf0e19d3f1dec7a77e36bdf8f7e00
Paul Otieno <vitalispaul48@live.com>
2025-12-08 11:32:08 +0300

    Initial setup

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI
 from src.books.routes import book_router
+from src.db.main import initdb
+from contextlib import asynccontextmanager
+
+
+#the lifespan event
+@asynccontextmanager
+async def lifespan(app: FastAPI):    
+    await initdb()
+    print("Server is starting...")
+    yield
+    print("server is stopping")
+
 
 version = 'v1'
 
 app = FastAPI(
     title="Bookly",
     description="A RESTful API for a book review web service",
-    version=version
+    version=version,
+    lifespan=lifespan
 )
 
 app.include_router(
---
+++ src/db/main.py
@@ -0,0 +1,22 @@
+import os
+from sqlmodel import create_engine, text
+from sqlalchemy.ext.asyncio import AsyncEngine
+from src.config import Config
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = AsyncEngine(create_engine(
+    url=Config.DATABASE_URL,
+    echo=True
+))
+
+
+async def initdb():
+    """create a connection to our db"""
+
+    async with engine.begin() as conn:
+        statement = text("select 'Hello World'")
+
+        result = await conn.execute(statement)
+
+        print(result)
